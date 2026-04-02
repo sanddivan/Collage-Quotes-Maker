32dc5571c1697e515871108c9730f053551e46d9
Ivan Kastle <151877949+sanddivan@users.noreply.github.com>
2026-04-01 19:41:46 -0700

    Laid the basis for the Canvas component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // File: App.jsx
 
+import Canvas from "./components/Canvas.jsx";
+
 /**
  * Setup function!
  */
 
 export default function App() {
-
+    return (
+        <div className="app">
+            <Canvas width="1280" height="720" />
+        </div>
+    );
 }
---
+++ src/components/Canvas.jsx
@@ -0,0 +1,17 @@
+// File: Canvas.jsx
+
+import styles from "./Canvas.module.css";
+
+export default function Canvas({ width, height }) {
+    /** @type {React.CSSProperties} */
+    const canvasDimensionsCss = {
+        '--canvas-width': `${width}px`,
+        '--canvas-height': `${height}px`
+    }
+
+    return (
+        <div className={styles.workspace}>
+            <div className={styles.theCanvas} style={canvasDimensionsCss}></div>
+        </div>
+    );
+}
