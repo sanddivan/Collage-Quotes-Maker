4737282638444fd32dd5ad1d1a3f5c5f08d9f66f
Ivan Kastle <151877949+sanddivan@users.noreply.github.com>
2026-04-16 15:19:52 -0700

    Trying again one more time to make this work.

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// File: main.jsx
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App.jsx";
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
