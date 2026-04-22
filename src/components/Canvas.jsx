0837c09a8c90b5a916f49adc8f71f467e3a748c0
Ivan Kastle <151877949+sanddivan@users.noreply.github.com>
2026-04-21 18:44:15 -0700

    Wrote down next steps.

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -16,6 +16,17 @@ export default function Canvas() {
     const layoutDesc = baseLayoutData[layout];
     const slotsData = layoutDesc['slots'];
 
+    // Create a hook to use here, where we map each slot's data to an actual
+    // set of points, by applying Maths to the shapes' data.
+    //
+    // This hook should receive the layout data, and return the array of slots.
+    //
+    // Later on, inside that hook, we will call another hook that applies the
+    // inset polygon calculations for when there is spacing between slots.
+    //
+    // We'll also need an ImageSlot.module.css, where we'll implement the
+    // slots' animations.
+
     const theSlots = slotsData.map((sData) => {
         const sShape = sData['shape'];
         const shapeData = shapesData[sShape];
---
+++ src/components/ImageSlot.jsx
@@ -0,0 +1,9 @@
+// File: ImageSlot.jsx
+
+/**
+ * @returns {React.JSX.Element}
+ */
+
+export default function ImageSlot() {
+    return (<div></div>);
+}
